8e11a525a6a2e245b761d92414975788b9ff4cc5
beloglazovigor <39671869+beloglazovigor@users.noreply.github.com>
2019-03-22 16:10:30 +0300

    create project

---
+++ authapp/tests.py
@@ -0,0 +1,12 @@
+# from django.test import TestCase
+import hashlib
+import random
+
+email = 'test@test.com'
+
+salt = hashlib.sha1(str(random.random()).encode('utf8')).hexdigest()[:6]
+activation_key = hashlib.sha1((email + salt).encode('utf8')).hexdigest()
+
+print(activation_key)
+
+
